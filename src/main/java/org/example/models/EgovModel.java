ac5c9af3bdac5a5337db4f4b290a03a167b7aa22
kanishq-egov <kanishq.bhatnagar@egovernments.org>
2024-03-12 14:57:07 +0530

    updated to version 2.0 using super builder

--- src/main/java/org/example/models/EgovModel.java
+++ src/main/java/org/example/models/EgovModel.java
@@ -2,21 +2,22 @@ package org.example.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class EgovModel {
     protected String id;
     protected String tenantId;
-//    protected AdditionalFields additionalFields;
-    protected Boolean isDeleted = Boolean.FALSE; //-> status obj
+    protected Boolean isDeleted = Boolean.FALSE;
     protected Integer rowVersion;
     protected String applicationId;
     protected Boolean hasErrors = Boolean.FALSE;
-//    protected AuditDetails auditDetails;
 }
--- src/main/java/org/example/models/EgovOfflineModel.java
+++ src/main/java/org/example/models/EgovOfflineModel.java
@@ -2,18 +2,17 @@ package org.example.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
+@SuperBuilder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class EgovOfflineModel extends EgovModel {
     protected String clientReferenceId;
-
 }
